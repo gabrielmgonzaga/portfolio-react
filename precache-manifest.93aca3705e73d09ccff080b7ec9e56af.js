beaa4836b74f980132fc469b01a2104f1e83ac46
gabrielgonzaga <gabrielgonzaga@gabrielagasimac.attlocal.net>
2019-12-05 08:57:52 -0800

    Updates

---
+++ precache-manifest.93aca3705e73d09ccff080b7ec9e56af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908ce2c5dfdd2d94bcf779fa9802e620",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "aee48898eba84288523a",
+    "url": "/portfolio-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b871e4ef1228ea41f852",
+    "url": "/portfolio-react/static/js/2.7494cadc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio-react/static/js/2.7494cadc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aee48898eba84288523a",
+    "url": "/portfolio-react/static/js/main.ddd26799.chunk.js"
+  },
+  {
+    "revision": "3477add3972f5d64e10e",
+    "url": "/portfolio-react/static/js/runtime-main.aea9bebe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
