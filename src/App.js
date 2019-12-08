059df3ae5cef21d990f9e6302ee36a08bf38f0c3
gabrielgonzaga <gabrielmgonzaga@gmail.com>
2019-12-08 10:41:40 -0800

    add nav component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './component/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Coming Soon...
-        </p>
-      </header>
+      <Nav />
     </div>
   );
 }
---
+++ src/component/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Nav() {
+    return (
+        <nav>
+            <h1>GG</h1>
+            <div className="hamburger">&#9776;</div>
+            <ul class="menu">
+                <li>Home</li>
+                <li>About</li>
+                <li>Blog</li>
+            </ul>
+        </nav>
+    )
+}
