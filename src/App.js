5dc253088972bd693b9aa27597dea0d60368e6fa
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2020-01-14 19:48:42 -0800

    Add work cards

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-} from "react-router-dom";
+} from 'react-router-dom';
 
 import Nav from './components/Nav';
 import Work from './pages/Work';
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+
+const card = [
+    {
+        id: 0,
+        link: '/',
+        imgSrc: 'https://via.placeholder.com/1000',
+        imgAlt: 'Image Description 2',
+        title: 'Case Study 1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id: 1,
+        link: '/',
+        imgSrc: 'https://via.placeholder.com/1000',
+        imgAlt: 'Image Description 2', 
+        title: 'Case Study 2',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+        id: 2,
+        link: '/',
+        imgSrc: 'https://via.placeholder.com/1000',
+        imgAlt: 'Image Description 2', 
+        title: 'Case Study 3',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+]
 
 export default function CaseStudies() {
     return (
@@ -10,7 +38,18 @@ export default function CaseStudies() {
                 <title>Gabriel Gonzaga - UX Work</title>
             </Helmet>
 
-            Under Construction 🚧
+            <h2>Work</h2>
+            <div className="work">
+                {card.map(i => 
+                    <div className="work-card" key={i.id}>
+                        <Link to={i.link}>
+                            <img src={i.imgSrc} alt={i.imgAlt}/>
+                            <h3>{i.title}</h3>
+                            <p>{i.description}</p>
+                        </Link>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
