2287fedafdd6f9f07c13387881f5cc1f3f336cfe
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2020-02-03 17:46:51 -0800

    update homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,11 +13,11 @@ export default function Home() {
             </Helmet>
 
             <h2>Hello  <span role="img" aria-label="high-five">👋</span></h2>
-            <p>I'm Gabriel, a <a rel="noopener noreferrer" target="_blank" href="https://github.com/gabrielmgonzaga">Web Developer</a> based in Irvine. 
-                Lately, I've been focused on modern JavaScript Development and UI/UX Design.
-            </p> 
+            <p>I'm Gabriel, a <a rel="noopener noreferrer" target="_blank" href="https://github.com/gabrielmgonzaga">Front End Developer</a> based in Irvine.
+                Lately, I've been focused on JavaScript Development and UI/UX Design.
+            </p>
             <p>
-                Please check out some of my <Link to="/work">work</Link> and profile links below. If you have any questions, <a href="mailto:gabrielmgonzaga@gmail.com">reach out</a> anytime. 
+                Please check out some of my <Link to="/work">work</Link> and profile links below. If you have any questions, <a href="mailto:gabrielmgonzaga@gmail.com">reach out</a> anytime.
             </p>
         </div>
     )
